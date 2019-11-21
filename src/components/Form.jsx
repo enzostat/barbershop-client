61866c748b7f5946dc49cfb406078b2288125346
Stanley Staton <stanleystaton@Stanleys-MacBook-Pro-2.local>
2019-11-20 16:14:59 -0800

    form

---
+++ src/components/Form.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { placeholder } from '@babel/types'
+
+
+const Form = props => {
+    return (
+        <div>
+            This is the Form
+            <form>
+
+                <input />
+                <input />
+                <input />
+                <input />
+            </form>
+        </div>
+    )
+}
+
+export default Form
